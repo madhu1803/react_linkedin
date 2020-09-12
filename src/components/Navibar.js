a979fc0edb3c54b1e310884e6ba99dba9001b7c4
madhumithaa <madhumithaa1411@gmail.com>
2020-09-12 21:25:59 +0530

    navleft half done

--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -1,24 +1,26 @@
 import React, { Component } from "react";
-import {
-  Nav,
-  NavDropdown,
-  Navbar,
-  Form,
-  Button,
-  FormControl,
-} from "react-bootstrap";
+import { Nav, NavDropdown, Navbar, Form, FormControl } from "react-bootstrap";
+import { AiFillLinkedin } from "react-icons/ai";
 
 export default class Navibar extends Component {
   render() {
     return (
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar expand="lg" className="navbar-container ">
+        <Navbar.Brand href="#home">
+          <AiFillLinkedin className="linkedin-icon" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          </Form>
           <Nav className="mr-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -28,12 +30,20 @@ export default class Navibar extends Component {
               <NavDropdown.Item href="#action/3.4">
                 Separated link
               </NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
+            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown> */}
+            <Nav.Link href="#home">Try premium for free</Nav.Link>
           </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-          </Form>
         </Navbar.Collapse>
       </Navbar>
     );
